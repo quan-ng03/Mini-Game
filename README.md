3fb38310bf1082d38c969499a6a1aefe3c50d431
Quan Nguyen <jacob.nguyenaq@gmail.com>
2023-05-16 01:32:26 -0700

    commit

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# TikTakToe
-A game known by every soul existing on Earth
+# Mini-game Page
+
+A list of games known by every soul existing on Earth
 
 Made using HTML, CSS, and JavaScript. This is my first project, expecting that there will be more advanced projects in the future during summer. As I am gravitating toward being a software engineer in the near future. This is my first stepping stone.
 
-Enjoy the game!
+Enjoy the games!
--- script.js
+++ script.js
@@ -1,76 +1,20 @@
-const cells = document.querySelectorAll('.cell');
-const statusText = document.querySelector('#statusText');
-const restartBtn = document.querySelector('#restartBtn');
-const winConditions = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-let options = ['', '', '', '', '', '', '', '', ''];
-let currentPlayer = 'X';
-let running = false;
+const games = document.querySelectorAll('.game');
 
-initializeGame();
+homeFunction();
 
-function initializeGame() {
-  cells.forEach((cell) => cell.addEventListener('click', cellClicked));
-  restartBtn.addEventListener('click', restartGame);
-  statusText.textContent = `${currentPlayer}'s turn`;
-  running = true;
+function homeFunction() {
+  games.forEach((game) => game.addEventListener('click', startGame));
 }
-function cellClicked() {
-  const cellIndex = this.getAttribute('cellIndex');
 
-  if (options[cellIndex] != '' || !running) {
-    return;
+function startGame() {
+  const gameNums = this.getAttribute('gameNum');
+  if (gameNums == 1) {
+    setTimeout(function () {
+      window.location.href = './ttt.html';
+    }, 3000);
+  } else if (gameNums == 2) {
+    setTimeout(function () {
+      window.location.href = './rps.html';
+    }, 3000);
   }
-
-  updateCell(this, cellIndex);
-  checkWinner();
-}
-function updateCell(cell, index) {
-  options[index] = currentPlayer;
-  cell.textContent = currentPlayer;
-}
-function changePlayer() {
-  currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
-  statusText.textContent = `${currentPlayer}'s turn`;
-}
-function checkWinner() {
-  let roundWon = false;
-
-  for (let i = 0; i < winConditions.length; i++) {
-    const condition = winConditions[i];
-    const cellA = options[condition[0]];
-    const cellB = options[condition[1]];
-    const cellC = options[condition[2]];
-    if (cellA == '' || cellB == '' || cellC == '') {
-      continue;
-    }
-    if (cellA == cellB && cellA == cellC) {
-      roundWon = true;
-      break;
-    }
-  }
-  if (roundWon) {
-    running = false;
-    statusText.textContent = `${currentPlayer} Won!!!`;
-  } else if (!options.includes('')) {
-    running = false;
-    statusText.textContent = `It's a draw -_-`;
-  } else {
-    changePlayer();
-  }
-}
-function restartGame() {
-  currentPlayer = 'X';
-  options = ['', '', '', '', '', '', '', '', ''];
-  statusText.textContent = `${currentPlayer}'s turn`;
-  cells.forEach((cell) => (cell.textContent = ''));
-  running = true;
 }
