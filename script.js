e2b5cfd3477dff4331716031afa55e8e74979e90
Quan Nguyen <jacob.nguyenaq@gmail.com>
2023-05-26 02:49:31 -0700

    commit

--- script.js
+++ script.js
@@ -9,12 +9,22 @@ function homeFunction() {
 function startGame() {
   const gameNums = this.getAttribute('gameNum');
   if (gameNums == 1) {
+    document.body.style.cursor = 'wait';
     setTimeout(function () {
       window.location.href = './ttt.html';
     }, 3000);
+    document.body.style.cursor = 'wait';
+    window.onload = function () {
+      document.body.style.cursor = 'default';
+    };
   } else if (gameNums == 2) {
+    document.body.style.cursor = 'wait';
     setTimeout(function () {
-      window.location.href = './rps.html';
+      window.location.href = './ttt.html';
     }, 3000);
+    document.body.style.cursor = 'wait';
+    window.onload = function () {
+      document.body.style.cursor = 'default';
+    };
   }
 }
